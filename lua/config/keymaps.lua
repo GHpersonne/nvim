b57f997e3012b9def0b0ad7cfc855db5a546049b
Anthony Jambon <anjambon@z3r4p4.42lyon.fr>
2025-10-02 20:04:45 +0200

    restore starter lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default ig/keymaps.lua
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- Split terminal in a new window
--- This file is automatically loaded by lazyvim.config.init
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
-local opt = vim.opt
-
-opt.expandtab = false -- Use spaces instead of tabs
-opt.tabstop = 4 -- Number of spaces tabs count for
